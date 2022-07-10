23ee4ee3ef37e94daeb1622d95903646c1f3efa6
marcos <manmoura@yahoo.com>
2022-07-11 02:58:25 +1200

    Added validations for invalid amount of money to get notes and documentation.

--- src/main/java/nz/co/snapper/backendinterview/controller/ATMController.java
+++ src/main/java/nz/co/snapper/backendinterview/controller/ATMController.java
@@ -2,6 +2,8 @@ package nz.co.snapper.backendinterview.controller;
 
 import nz.co.snapper.backendinterview.service.ATMService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +13,10 @@ public class ATMController {
 
     @Autowired
     private ATMService atmService;
+
+    @GetMapping(path = {"/{value}"})
+    public int[] cashValue(@PathVariable(name="value") Integer value) {
+        return atmService.getNotes(value);
+    }
 
 }
