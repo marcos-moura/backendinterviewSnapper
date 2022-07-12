855ebf6d5fbcb4b186f399d2e586162f69d6ce4d
marcos <manmoura@yahoo.com>
2022-07-13 07:40:27 +1200

    Improving Tests; Added response with an easy-to-count notes response; Removed exception for amount value 0 to return an empty list, and for negative values. Improved documentation.

--- src/main/java/nz/co/snapper/backendinterview/service/ATMService.java
+++ src/main/java/nz/co/snapper/backendinterview/service/ATMService.java
@@ -8,7 +8,6 @@ import java.util.*;
 public class ATMService {
 
     private static final Integer[] VALID_NOTES = {5, 10, 20, 50, 100};
-    private static final int MIN_NOTE_VALUE = 5;
 
     /**
      * Get the notes according to the value to be cashed.
@@ -19,11 +18,12 @@ public class ATMService {
      */
     public List<Integer> getNotes(long amountToCash) {
 
-        if (amountToCash == 0) {
-            throw new UnsupportedOperationException("Value to cash can not be 0");
+        if (amountToCash <= 0) {
+            return new ArrayList<>();
         }
 
-        if (amountToCash % MIN_NOTE_VALUE > 0) {
+        Integer minimumNoteValue = Collections.min(Arrays.asList(VALID_NOTES));
+        if (amountToCash % minimumNoteValue > 0) {
             throw new UnsupportedOperationException("Value to cash can only be multiple of 5");
         }
 
@@ -43,7 +43,7 @@ public class ATMService {
         if (amountToCash > notesValueSum){
 
             notesToCash.addAll(Arrays.asList(VALID_NOTES));
-            minimalNotesMap = getMinimalNotes(4, amountToCash - notesValueSum, new HashMap<>());
+            minimalNotesMap = getMinimalNotes(VALID_NOTES.length - 1, amountToCash - notesValueSum, new HashMap<>());
             notesToCash.addAll(getNotesAsList(minimalNotesMap));
         } else {
 
@@ -57,7 +57,7 @@ public class ATMService {
 
                 } else if (remainingValue > 0) {
 
-                    minimalNotesMap = getMinimalNotes(4, remainingValue, new HashMap<>());
+                    minimalNotesMap = getMinimalNotes(VALID_NOTES.length - 1, remainingValue, new HashMap<>());
                     List<Integer> notesList = getNotesAsList(minimalNotesMap);
                     notesToCash.addAll(notesList);
                     break;
@@ -67,10 +67,15 @@ public class ATMService {
             }
         }
 
+        // Easy to count notes
+        notesToCash.sort(Collections.reverseOrder());
         return notesToCash;
 
     }
 
+    /**
+     * Return a list of notes given a map with number of notes per notes value.
+     */
     private List<Integer> getNotesAsList(Map<Integer, Integer> minimalNotesMap) {
         List<Integer> notesList =  new ArrayList<>();
         minimalNotesMap.forEach((noteValue, quantityOfNotes) -> {
@@ -81,6 +86,9 @@ public class ATMService {
         return notesList;
     }
 
+    /**
+     * Create a Map with as few notes as possible.
+     */
     private Map<Integer, Integer> getMinimalNotes(int noteIndex, long amountToCash, Map<Integer, Integer> notesToCash) {
 
         long notes = amountToCash / VALID_NOTES[noteIndex];
@@ -98,25 +106,3 @@ public class ATMService {
     }
 }
 
-//    /**
-//     *
-//     * Calculates the number of notes to obtain as many as unique notes as possible.
-//     * It iterates through valid notes Array from the biggest value to small value until get the
-//     * first remaining value equal 0 to return.
-//     *
-//     */
-//    private List<Integer> getUniqueNotes(int noteIndex, int amountToCash, int[] notesToCash) {
-//
-//        int notes = amountToCash / VALID_NOTES[noteIndex];
-//        int remainingValue = amountToCash % VALID_NOTES[noteIndex] ;
-//
-//        if (remainingValue == 0) {
-//            notesToCash[0] = VALID_NOTES[noteIndex];
-//            notesToCash[1] = notes;
-//        } else {
-//            getUniqueNotes(++noteIndex, amountToCash, notesToCash);
-//        }
-//
-//        return notesToCash;
-//    }
-//}
--- src/test/java/nz/co/snapper/backendinterview/service/ATMServiceTest.java
+++ src/test/java/nz/co/snapper/backendinterview/service/ATMServiceTest.java
@@ -2,21 +2,16 @@ package nz.co.snapper.backendinterview.service;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ATMServiceTest {
 
     ATMService atmService = new ATMService();
-    @Test
-    void given_value_zero_should_return_exception() {
-        String expectedMessage = "Value to cash can not be 0";
-        try {
-            atmService.getNotes(0);
-        } catch (UnsupportedOperationException exception) {
-            assertEquals(expectedMessage, exception.getMessage());
-        }
-
-    }
 
     @Test
     void given_value_not_multiple_of_5_should_return_exception() {
@@ -29,12 +24,33 @@ class ATMServiceTest {
     }
 
     @Test
-    void given_valid_amount_should_return_max_unique_notes() {
+    void given_zero_or_negative_value_should_return_empty_list() {
 
-//        assertEquals(1, atmService.getNotes(5)[1]);
-//        assertEquals(2, atmService.getNotes(40)[1]);
-//        assertEquals(3, atmService.getNotes(150)[1]);
-//        assertEquals(29, atmService.getNotes(290)[1]);
-//        assertEquals(59, atmService.getNotes(295)[1]);
+        assertTrue(atmService.getNotes(0).isEmpty());
+        assertTrue(atmService.getNotes(-50).isEmpty());
+
+    }
+
+    @Test
+    void given_valid_amount_request_should_sum_of_notes_be_equal() {
+
+        int expectedValue = 90;
+        List<Integer> notes = atmService.getNotes(90);
+
+        int notesValueSum = notes.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+        assertEquals(expectedValue, notesValueSum);
+
+    }
+
+    @Test
+    void given_valid_amount_185_should_return_only_unique_notes() {
+
+        Integer[] NOTES = {5, 10, 20, 50, 100};
+        List<Integer> expectedNotes = Arrays.asList(NOTES);
+
+        List<Integer> notesCashed = atmService.getNotes(185);
+        assertTrue(expectedNotes.containsAll(notesCashed));
     }
 }
